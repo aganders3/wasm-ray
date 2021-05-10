40e27701c1ba788a31f1b7407a91dce858063e12
aganders3 <aganders3@users.noreply.github.com>
2021-05-10 19:14:59 +0000

    deploy: 97bbdd9d10c9d6877c2689f9a6c0788f22ee0476

--- bootstrap.js
+++ bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_ray_bg.wasm":"005e05612b504381589f"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_ray_bg.wasm":"578144b144b677378d0d"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
